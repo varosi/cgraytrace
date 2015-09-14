3909cff543fd9d99e4211aea56b9b173cb3905f2
Vassil Keremidchiev <varosi@gmail.com>
2015-09-14 11:28:18 -0700

    Little web server to show us rendering result

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,17 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE QuasiQuotes               #-}
+{-# LANGUAGE TemplateHaskell           #-}
+{-# LANGUAGE TypeFamilies              #-}
+
 module Main where
 
+-- http://hackage.haskell.org/package/linear
+
+import Yesod
 import Codec.Picture.Png
 import Codec.Picture.Types (Image(..), PixelRGB8)
 import Data.Vector.Storable (generate)
+import Linear.V3
 
 image :: Image PixelRGB8
 image = Image width height myData where
@@ -10,6 +19,14 @@ image = Image width height myData where
     width  = 320
     height = 240
 
+-- Little server to show us rendering result
+data App = App
+instance Yesod App
+
+mkYesod "App" [parseRoutes| / ImageR GET |]
+
+getImageR :: MonadHandler m => m TypedContent
+getImageR = sendResponse $ toTypedContent (typePng, toContent (encodePng image))
+
 main :: IO ()
-main = do
-    writePng "test.png" image
+main = warpEnv App
