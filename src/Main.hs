f8555b715e5da7ce2d6288c1e0d42be40f3a4f27
Vassil Keremidchiev <varosi@gmail.com>
2015-09-14 12:54:19 -0700

    Fixed some warnings

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ module Main where
 import Yesod
 import Codec.Picture.Png
 import Raytracer
+import Linear.V3
 
 -- Little server to show us rendering result
 data App = App
@@ -17,7 +18,9 @@ mkYesod "App" [parseRoutes| / ImageR GET |]
 
 getImageR :: MonadHandler m => m TypedContent
 getImageR = sendResponse $ toTypedContent (typePng, toContent (encodePng image)) where
-                image = raytrace [] . Camera . Sensor $ (320, 240)
+                image  = raytrace [] $ Camera sensor camPos
+                sensor = Sensor (320, 240)
+                camPos = Coord3 $ V3 0.0 0.0 0.0 :: Coord3 Float
 
 main :: IO ()
 main = warpEnv App
--- src/Raytracer.hs
+++ src/Raytracer.hs
@@ -1,24 +1,31 @@
-module Raytracer( raytrace, Entity(..), Scene(..), Sensor(..), Camera(..) ) where
+module Raytracer( raytrace, Entity, Scene, Sensor(..), Camera(..), Coord3(..) ) where
 
 import Codec.Picture.Types (Image(..), PixelRGB8(..), generateImage)
-import Data.Vector.Storable (generate)
 import Linear.V2
+import Linear.V3
 
 -- http://hackage.haskell.org/package/linear
 
 type Entity = Int
 type Scene  = [Entity]
 
-newtype ScreenSpace a = SS (V2 a)
+newtype ScreenSpace a = SS     (V2 a)
+newtype Coord3 r      = Coord3 (V3 r)
 
 newtype Sensor = Sensor (Int, Int) -- width, height
-newtype Camera = Camera Sensor
+data Camera r  = Camera Sensor (Coord3 r)
+
+-- Image space to screen space
+toScreenSpace :: (Integral i, Fractional f) => Sensor -> i -> i -> ScreenSpace f
+toScreenSpace (Sensor (width, height)) x y = SS $ V2 sx sy where
+        sx = fromIntegral x / (fromIntegral width  - 1)
+        sy = fromIntegral y / (fromIntegral height - 1)
 
 -- Single sample
 sample :: Scene -> ScreenSpace Float -> PixelRGB8
-sample _ (SS (V2 x y)) = PixelRGB8 a 0 0 where a = if x < 50 then 0 else 0xff
+sample _ (SS (V2 x y)) = PixelRGB8 a 0 0 where a = if x < 0.5 then 0 else 0xff
 
 -- Ray-trace whole image viewed by camera
-raytrace :: Scene -> Camera -> Image PixelRGB8
-raytrace scene (Camera (Sensor (width, height))) = generateImage imageSample width height where
-    imageSample x y = sample scene (SS (V2 (fromIntegral x) (fromIntegral y)))
+raytrace :: Fractional r => Scene -> Camera r -> Image PixelRGB8
+raytrace scene (Camera sensor@(Sensor (width, height)) _) = generateImage imageSample width height where
+    imageSample x y = sample scene $ toScreenSpace sensor x y
