962e82b6893c46b8f786540b151d2b8696c5f21f
Vassil Keremidchiev <varosi@gmail.com>
2015-09-15 12:34:31 -0700

    Added Ray and Energy

--- src/Raytracer.hs
+++ src/Raytracer.hs
@@ -3,6 +3,7 @@ module Raytracer( raytrace, Entity, Scene, Sensor(..), Camera(..), Coord3(..) ) 
 import Codec.Picture.Types (Image(..), PixelRGB8(..), generateImage)
 import Linear.V2
 import Linear.V3
+import Data.Word8
 
 -- http://hackage.haskell.org/package/linear
 
@@ -12,8 +13,19 @@ type Scene  = [Entity]
 newtype ScreenSpace a = SS     (V2 a)
 newtype Coord3 r      = Coord3 (V3 r)
 
-newtype Sensor = Sensor (Int, Int) -- width, height
+newtype Ray r         = Ray (Coord3 r, V3 r) -- position & direction
+
+newtype Energy        = Energy (Float, Float, Float) -- R, G, B components of energy that we sense
+
+newtype Sensor = Sensor (Int, Int)           -- width, height
 data Camera r  = Camera Sensor (Coord3 r)
+
+cameraRay :: Camera r -> ScreenSpace r -> Ray r
+cameraRay = undefined
+
+mapEnergy :: Energy -> PixelRGB8
+mapEnergy (Energy (r, g, b)) = PixelRGB8 (f2w r) (f2w g) (f2w b) where
+        f2w f = truncate (f * 255.0)
 
 -- Image space to screen space
 toScreenSpace :: (Integral i, Fractional f) => Sensor -> i -> i -> ScreenSpace f
@@ -22,10 +34,10 @@ toScreenSpace (Sensor (width, height)) x y = SS $ V2 sx sy where
         sy = fromIntegral y / (fromIntegral height - 1)
 
 -- Single sample
-sample :: Scene -> ScreenSpace Float -> PixelRGB8
-sample _ (SS (V2 x y)) = PixelRGB8 a 0 0 where a = if x < 0.5 then 0 else 0xff
+sample :: Scene -> ScreenSpace Float -> Energy
+sample _ (SS (V2 x y)) = Energy (a, 0.0, 0.0) where a = if x < 0.5 then 0.0 else 1.0
 
 -- Ray-trace whole image viewed by camera
 raytrace :: Fractional r => Scene -> Camera r -> Image PixelRGB8
 raytrace scene (Camera sensor@(Sensor (width, height)) _) = generateImage imageSample width height where
-    imageSample x y = sample scene $ toScreenSpace sensor x y
+    imageSample x y = mapEnergy . sample scene $ toScreenSpace sensor x y
