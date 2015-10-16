b7fd85f3fb6dd9b62b17d3a064dae402d3898aa0
Vassil Keremidchiev <varosi@gmail.com>
2015-10-16 11:26:04 -0700

    Leave only System.Random.TF.Gen random generator as fastest so far.

--- src/Main.hs
+++ src/Main.hs
@@ -11,9 +11,7 @@ import Yesod
 import Codec.Picture.Png
 import Raytracer
 import Scene
-import System.Random.TF.Gen (seedTFGen)
--- import System.Random (RandomGen(..))
--- import System.Random.Mersenne.Pure64( newPureMT )
+import System.Random.TF.Gen (seedTFGen, RandomGen(..))
 
 -- Little server to show us rendering result
 data App = App
@@ -23,8 +21,6 @@ mkYesod "App" [parseRoutes| / ImageR GET |]
 
 getImageR :: MonadHandler m => m TypedContent
 getImageR = do
-                --gen <- liftIO newPureMT
-                -- gen <- liftIO newStdGen
                 let gen = seedTFGen (1,2,3,4)
                 let image = raytrace gen cornellScene cornellCamera
                 sendResponse $ toTypedContent (typePng, toContent (encodePng image))
