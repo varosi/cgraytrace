2fb02fa484f34ec73c3347a25d66cce9b584e787
Vassil Keremidchiev <varosi@gmail.com>
2015-10-23 11:11:31 -0700

    Even more comments

--- src/Raytracer.hs
+++ src/Raytracer.hs
@@ -42,17 +42,22 @@ pathTrace :: RandomGen gen => gen -> Scene -> RaySegment -> (LightIntensity, gen
 pathTrace gen scene = pathTrace' maxDepth Nothing gen where
     maxDepth = rsPathMaxDepth.scSettings $ scene
 
-    pathTrace' 0 _ g _                                                      = (scEnvLight scene, g)
-    pathTrace' levelsLeft prevHit g0 raySeg@(RaySeg (Ray (_, shootDir), _)) = ((+) <$> lightIntensity <*> giIntensity, g''') where
+    -- |Helper separation so we could reuse scene and initial conditions across recursive calls
+    pathTrace' 0 _ g _                      = (scEnvLight scene, g)
+    pathTrace' levelsLeft prevHit g0 raySeg = ((+) <$> lightIntensity <*> giIntensity, g''') where
+
+        (RaySeg (Ray (_, shootDir), _)) = raySeg
         geomHit        = traceRay scene prevHit raySeg
         dir2viewer     = normalize3( normalized shootDir ^* (-1) )
 
         isCameraRay    = maxDepth - levelsLeft == 0
         giSamplesCount = if isCameraRay then rsSecondaryGICount.scSettings $ scene else 1
 
+        -- Trace shadow rays and GI rays
         (lightIntensity, g'') = shootMany bounce2light (rsLightSamplesCount.scSettings $ scene) g0  (scEnvLight scene) geomHit     -- shadow rays shoot first
         (giIntensity, g''')   = shootMany bounce2GI    giSamplesCount                           g'' (scEnvLight scene) geomHit     -- gi rays shoot
 
+        -- |Shadow rays tracing
         bounce2light g hit@(Hit _ ipoint _ _ _ entity) = (reflectedLight, g') where
             light               = scLight scene
             (shadowRaySeg, g')  = shadowRay g light ipoint
@@ -65,12 +70,14 @@ pathTrace gen scene = pathTrace' maxDepth Nothing gen where
             irradiance  = eval light dir2light
             radiance    = evalHitBRDF hit irradiance dir2light
 
+        -- |GI tracing
         bounce2GI g hit@(Hit { isectEntity = entity }) = (exRadiance, gLast) where
-            (inRadiance, gLast)             = pathTrace' (levelsLeft-1) (Just entity) g' (RaySeg (nextRay, farthestDistance))
+            (inRadiance, gLast)             = pathTrace' (levelsLeft-1) (Just entity) g' (RaySeg (nextRay, farthestDistance))   -- recursive path tracing
             Mat brdf                        = enMaterial entity
             (nextRay@(Ray (_, inDir)), g')  = generateRay g brdf hit                -- from BRDF
             exRadiance                      = evalHitBRDF hit inRadiance inDir
 
+        -- |Evaluate BRDF of hit surfaces and return radiance
         evalHitBRDF hit irradiance irrDir = radiance where
             Mat brdf        = enMaterial . isectEntity $ hit
             brdfTransfer    = evalBRDF brdf hit dir2viewer irrDir
@@ -84,12 +91,16 @@ imageSample g scene camera = pathTrace g scene . cameraRay camera
 raytrace :: (RandomGen gen, Camera cam) => gen -> Scene -> cam -> Image PixelRGB8
 raytrace gen scene camera = Image width height raw where
     raw    = fromList( concatMap fromPixel pxList )
-    pxList = pixels gen `using` parBuffer 16 rseq   -- whole parallelism
 
+    -- |Introduce parallelism at this line
+    pxList = pixels gen `using` parBuffer 16 rseq
+
+    -- |Map light intensity to pixel color
     mapLightIntensity :: LightIntensity -> PixelRGB8
     mapLightIntensity (V3 r g b) = PixelRGB8 (f2w r) (f2w g) (f2w b) where
             f2w f = truncate (min 1 (((f /~ lumen) * exposure) ** invGamma) * 255)
 
+    -- |Raytraced pixels
     pixels gen' = map pixel $ zip (generators gen') (screenCoords sensor) where
             pixel (g, (x,y)) = mapLightIntensity . fst $ imageSample g scene camera $ toScreenSpace sensor x y
 
