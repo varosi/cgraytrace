4bdab72ab9c07dc7d04fd3ce76ffccaba765afc3
Vassil Keremidchiev <vasil.keremidchiev@chaosgroup.com>
2017-01-11 18:23:04 +0200

    Added stand-alone executable that is rendering to sample.png file in current directory

---
+++ App/Main.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Codec.Picture.Png
+import Raytracer
+import Scene
+import System.Random.TF.Gen (seedTFGen)
+
+main :: IO ()
+main = do
+    let gen = seedTFGen (1,2,3,4)
+    let image = raytrace gen cornellScene (cornellCamera 1024 768)
+    let filename = "sample.png"
+
+    putStrLn $ "Rendering to " ++ filename ++ "..."
+    writePng filename image
